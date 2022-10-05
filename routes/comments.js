6aeeb1dbf2235b9ccde4029a600a8efe29611637
minho park <kingpmh96@gamil.com>
2022-10-05 17:45:21 +0900

    update

--- routes/comments.js
+++ routes/comments.js
@@ -5,18 +5,18 @@ const router = express.Router(); // Router 객체 생성
 
 
 // 댓글 작성 API + 현지시간 필요
-router.post ("/:_postId", async (req, res) => {
+router.post ("/:_id", async (req, res) => {
    try{
      const createdAt = new Date();
      const date = createdAt.toLocaleDateString();
-    const { _postId } = req.params; // req.params_postId 구조분해할당한 형태
+    const { _id } = req.params; // req.params_postId 구조분해할당한 형태
 
     const { user, content, password } = req.body;
     console.log({ user, content, password });
 
     const writePost = await Comments.create(
         {
-          _postId,
+          _id,
             user,
             password,
             content,
@@ -33,7 +33,7 @@ router.post ("/:_postId", async (req, res) => {
 
 
 // 댓글 전체 조회 API (+내림차순 정렬 필요)
-router.get("/:_postId", async (req, res, next) => {
+router.get("/:_id", async (req, res, next) => {
     const comments = await Comments.find().sort("-createdAt");
 
     res.json({
@@ -42,35 +42,35 @@ router.get("/:_postId", async (req, res, next) => {
   });
 
 // 댓글 상세 조회 API
-router.get("/:_postId", async (req, res) => {
-    const { _postIdd } = req.params;
-    const detail = await Comments.findOne({ _postId });
+router.get("/:_id", async (req, res) => {
+    const { _id } = req.params;
+    const detail = await Comments.findOne({ _id });
   
     res.json({ detail });
   });
   
 
 // 댓글 삭제 API
-router.delete("/:_postId", async (req, res) => {
-     const { _postId } = req.params;
+router.delete("/:_id", async (req, res) => {
+     const { _id } = req.params;
     const { password } = req.body;
     const originPassword = await Comments.find({ password });
   
   
     if (password === String(originPassword[0].password)) {
-      await Comments.deleteOne({ _postId });
+      await Comments.deleteOne({ _id });
       return res.json({ message: "댓글을 삭제하였습니다." });
     }
     return res.json({ message: "비밀번호가 다릅니다." }); 
   });
   
   // 댓글 수정 API
-  router.put("/:_postId", async (req, res) => {
-    const { _postId } = req.params;
+  router.put("/:_id", async (req, res) => {
+    const { _id } = req.params;
     const { password, title, content } = req.body;
     const originPassword = await Comments.findOne({ password });
   if (originPassword){
-    await Comments.updateOne({ _postId }, { title, content });
+    await Comments.updateOne({ _id }, { title, content });
   }
   else {   
       return res.json({ message: "댓글을 수정하지 못했습니다." });
